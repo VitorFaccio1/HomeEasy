5b142e06c241455ac12f8ba5f11dd551c78e341d
Vitor Faccio <vitorfaccio6@gmail.com>
2022-06-16 14:22:58 -0300

    Adiciona nova migration e troca a connection string do banco de dados

--- HomeEasy/Migrations/20220615004429_InitialClient.Designer.cs
+++ HomeEasy/Migrations/20220615004429_InitialClient.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HomeEasy.Migrations
 {
     [DbContext(typeof(HomeEasyContext))]
-    partial class HomeEasyContextModelSnapshot : ModelSnapshot
+    [Migration("20220615004429_InitialClient")]
+    partial class InitialClient
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
