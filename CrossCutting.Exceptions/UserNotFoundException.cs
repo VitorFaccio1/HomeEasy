08da7f1bc4a43c7458db04d820de58d08547eb25
Vitor Faccio <vitorfaccio6@gmail.com>
2022-06-16 14:18:44 -0300

    Cria nova exception de user not found

---
+++ CrossCutting.Exceptions/UserNotFoundException.cs
@@ -0,0 +1,12 @@
+﻿using System.Net;
+
+namespace CrossCutting.Exceptions
+{
+    public sealed class UserNotFoundException : HttpCustomException
+    {
+        public UserNotFoundException(string message = "UserNotFoundException",
+            HttpStatusCode httpStatusCode = HttpStatusCode.NotFound) : base(message, httpStatusCode)
+        {
+        }
+    }
+}
