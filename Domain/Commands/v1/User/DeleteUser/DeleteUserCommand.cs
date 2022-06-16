898c6bcbd575bfad2ca5be5d6b523d39716de075
Vitor Faccio <vitorfaccio6@gmail.com>
2022-06-16 14:19:14 -0300

    Cria novo metodo de delete user

---
+++ Domain/Commands/v1/User/DeleteUser/DeleteUserCommand.cs
@@ -0,0 +1,17 @@
+﻿using MediatR;
+
+namespace HomeEasy.Domain.Commands.v1.User.DeleteUser
+{
+    public class DeleteUserCommand : IRequest
+    {
+        public DeleteUserCommand(long id)
+        {
+            Id = id;
+        }
+
+        /// <summary>
+        /// The user ID.
+        /// </summary>
+        public long Id { get; set; }
+    }
+}
---
+++ Domain/Commands/v1/User/DeleteUser/DeleteUserCommandHandler.cs
@@ -0,0 +1,38 @@
+﻿using CrossCutting.Exceptions;
+using Infrastructure.Data.Entities.User;
+using Infrastructure.Data.Interfaces;
+using MediatR;
+
+namespace HomeEasy.Domain.Commands.v1.User.DeleteUser
+{
+    public class DeleteUserCommandHandler : IRequestHandler<DeleteUserCommand, Unit>
+    {
+        private readonly IUserService _userService;
+
+        public DeleteUserCommandHandler(IUserService userService)
+        {
+            _userService = userService;
+        }
+
+        public async Task<Unit> Handle(DeleteUserCommand request, CancellationToken cancellationToken)
+        {
+            var user = await GetUser(request);
+
+            user.IsActive = false;
+
+            await _userService.UpdateUserAsync(user);
+
+            return Unit.Value;
+        }
+
+        private async Task<UserEntity> GetUser(DeleteUserCommand request)
+        {
+            var user = await _userService.GetUserAsync(request.Id);
+
+            if (user == null || user.IsActive == false)
+                throw new UserNotFoundException();
+
+            return user;
+        }
+    }
+}
