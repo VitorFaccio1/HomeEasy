fbb10599224532ad0073d13787e389d83f8434bc
Vitor Ravenna Faccio <5by5.vfaccio@voeazul.com.br>
2024-08-08 17:17:38 -0300

    Cria massa para testes

--- HomeEasy/Data/HomeEasyContext.cs
+++ HomeEasy/Data/HomeEasyContext.cs
@@ -5,33 +5,51 @@ namespace HomeEasy.Data;
 
 public sealed class HomeEasyContext : DbContext
 {
-    public HomeEasyContext(DbContextOptions<HomeEasyContext> options) : base(options)
-    {
-    }
+	public HomeEasyContext(DbContextOptions<HomeEasyContext> options) : base(options)
+	{
+	}
 
-    public DbSet<User> Users { get; set; } = default!;
+	public DbSet<User> Users { get; set; } = default!;
 
-    public DbSet<Job> Jobs { get; set; } = default!;
+	public DbSet<Job> Jobs { get; set; } = default!;
 
-    protected override void OnModelCreating(ModelBuilder modelBuilder)
-    {
-        base.OnModelCreating(modelBuilder);
+	public DbSet<Ad> Ads { get; set; } = default!;
 
-        modelBuilder.Entity<Job>().HasData(
-            new Job { Id = Guid.NewGuid(), Name = "Faxineiro(a)" },
-            new Job { Id = Guid.NewGuid(), Name = "Pintor(a)" },
-            new Job { Id = Guid.NewGuid(), Name = "Pedreiro(a)" }
-        );
+	protected override void OnModelCreating(ModelBuilder modelBuilder)
+	{
+		base.OnModelCreating(modelBuilder);
 
-        modelBuilder.Entity<User>().HasData(
-            new User
-            {
-                Id = Guid.NewGuid(),
-                Email = "admin@admin.com",
-                Password = "AQAAAAIAAYagAAAAEA9Zd9k/oCFtBcUMOt94/Brh110GdKXeDqeFMwNEbxVUv2Mlv5xcn+dxEn8fkCrIWw==",
-                Name = "Vitor Faccio",
-                Type = UserType.Admin
-            }
-        );
-    }
+		modelBuilder.Entity<Job>().HasData(
+			new Job { Id = Guid.NewGuid(), Name = "Faxineiro(a)" },
+			new Job { Id = Guid.NewGuid(), Name = "Pintor(a)" },
+			new Job { Id = Guid.NewGuid(), Name = "Pedreiro(a)" }
+		);
+
+		modelBuilder.Entity<User>().HasData(
+			new User
+			{
+				Id = Guid.NewGuid(),
+				Email = "vitor@admin.com",
+				Password = "AQAAAAIAAYagAAAAEA9Zd9k/oCFtBcUMOt94/Brh110GdKXeDqeFMwNEbxVUv2Mlv5xcn+dxEn8fkCrIWw==",
+				Name = "Vitor Faccio",
+				Type = UserType.Admin
+			},
+			new User
+			{
+				Id = Guid.NewGuid(),
+				Email = "vitor@worker.com",
+				Password = "AQAAAAIAAYagAAAAEA9Zd9k/oCFtBcUMOt94/Brh110GdKXeDqeFMwNEbxVUv2Mlv5xcn+dxEn8fkCrIWw==",
+				Name = "Vitor Faccio",
+				Type = UserType.Worker
+			},
+			new User
+			{
+				Id = Guid.NewGuid(),
+				Email = "vitor@client.com",
+				Password = "AQAAAAIAAYagAAAAEA9Zd9k/oCFtBcUMOt94/Brh110GdKXeDqeFMwNEbxVUv2Mlv5xcn+dxEn8fkCrIWw==",
+				Name = "Vitor Faccio",
+				Type = UserType.Client
+			}
+		);
+	}
 }
