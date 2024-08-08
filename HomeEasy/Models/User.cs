a86813435ed4a14d267d3681223fe343a7c6b786
Vitor Ravenna Faccio <5by5.vfaccio@voeazul.com.br>
2024-08-08 12:00:14 -0300

    User - Adiciona propriedade lista de Ads

--- HomeEasy/Models/User.cs
+++ HomeEasy/Models/User.cs
@@ -4,25 +4,27 @@ namespace HomeEasy.Models;
 
 public sealed class User
 {
-	[Key]
-	public Guid Id { get; set; }
+    [Key]
+    public Guid Id { get; set; }
 
-	[Display(Name = "E-mail")]
-	[Required(ErrorMessage = "Email é obrigatório")]
-	[StringLength(50, ErrorMessage = "Deve ter entre 5 e 50 caracteres", MinimumLength = 5)]
-	[RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", ErrorMessage = "Deve ser um e-mail válido")]
-	public string Email { get; set; }
+    [Display(Name = "E-mail")]
+    [Required(ErrorMessage = "Email é obrigatório")]
+    [StringLength(50, ErrorMessage = "Deve ter entre 5 e 50 caracteres", MinimumLength = 5)]
+    [RegularExpression("^[a-zA-Z0-9_.-]+@[a-zA-Z0-9-]+\\.[a-zA-Z0-9-.]+$", ErrorMessage = "Deve ser um e-mail válido")]
+    public string Email { get; set; }
 
-	[Display(Name = "Senha")]
-	[DataType(DataType.Password)]
-	[Required(ErrorMessage = "Senha é obrigatório")]
-	[StringLength(255, ErrorMessage = "Deve ser entre 5 e 255 caracteres", MinimumLength = 5)]
-	[RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", ErrorMessage = "A senha deve ter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula, um dígito e um caractere especial.")]
-	public string Password { get; set; }
+    [Display(Name = "Senha")]
+    [DataType(DataType.Password)]
+    [Required(ErrorMessage = "Senha é obrigatório")]
+    [StringLength(255, ErrorMessage = "Deve ser entre 5 e 255 caracteres", MinimumLength = 5)]
+    [RegularExpression("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", ErrorMessage = "A senha deve ter pelo menos 8 caracteres, uma letra maiúscula, uma letra minúscula, um dígito e um caractere especial.")]
+    public string Password { get; set; }
 
-	[Display(Name = "Nome")]
-	[Required(ErrorMessage = "Nome é obrigatório")]
-	public string Name { get; set; }
+    [Display(Name = "Nome")]
+    [Required(ErrorMessage = "Nome é obrigatório")]
+    public string Name { get; set; }
 
-	public UserType Type { get; set; }
+    public UserType Type { get; set; }
+
+    public List<Ad> Ads { get; set; } = [];
 }
