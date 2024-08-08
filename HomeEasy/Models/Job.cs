33c814a4bc4798b09587c849d1b9e8d1967df1f1
Vitor Ravenna Faccio <5by5.vfaccio@voeazul.com.br>
2024-08-08 11:59:13 -0300

    Job - Adiciona Anottation Required e Display na propriedade Name

--- HomeEasy/Models/Job.cs
+++ HomeEasy/Models/Job.cs
@@ -7,5 +7,7 @@ public sealed class Job
     [Key]
     public Guid Id { get; set; }
 
+    [Required(ErrorMessage = "Nome é obrigatório")]
+    [Display(Name = "Nome")]
     public string Name { get; set; }
 }
