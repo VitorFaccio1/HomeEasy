2aaf0893f587e4edc7192c2972baaa54656737de
Vitor Ravenna Faccio <5by5.vfaccio@voeazul.com.br>
2024-08-08 17:18:17 -0300

    Cacheia GetJobsAsync

--- HomeEasy/Services/JobService.cs
+++ HomeEasy/Services/JobService.cs
@@ -2,21 +2,36 @@
 using HomeEasy.Interfaces;
 using HomeEasy.Models;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Caching.Memory;
 
 namespace HomeEasy.Services;
 
 public sealed class JobService : IJobService
 {
+    private readonly IMemoryCache _cache;
     private readonly HomeEasyContext _context;
 
-    public JobService(HomeEasyContext context)
+    public JobService(HomeEasyContext context, IMemoryCache cache)
     {
+        _cache = cache;
         _context = context;
     }
 
     public async Task<List<Job>> GetJobsAsync()
     {
-        return await _context.Jobs.ToListAsync();
+        var jobsCacheKey = "jobs";
+
+        if (!_cache.TryGetValue(jobsCacheKey, out List<Job> jobs))
+        {
+            jobs = await _context.Jobs.ToListAsync();
+
+            _cache.Set(jobsCacheKey, jobs, new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromHours(24)
+            });
+        }
+
+        return jobs;
     }
 
     public async Task<Job> GetJobAsync(Guid? id)
