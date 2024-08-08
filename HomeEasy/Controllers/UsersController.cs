0639c22532d9abe8b5502545171fa1d33758420d
Vitor Ravenna Faccio <5by5.vfaccio@voeazul.com.br>
2024-08-08 17:17:27 -0300

    Cria novo metodo GetWorkers

--- HomeEasy/Controllers/UsersController.cs
+++ HomeEasy/Controllers/UsersController.cs
@@ -1,81 +1,92 @@
-﻿using System.Security.Claims;
-using HomeEasy.Interfaces;
+﻿using HomeEasy.Interfaces;
 using HomeEasy.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
 
 namespace HomeEasy.Controllers;
 
 public sealed class UsersController : Controller
 {
-	private readonly IUserService _userService;
+    private readonly IUserService _userService;
+    private readonly IJobService _jobService;
 
-	public UsersController(IUserService userService)
-	{
-		_userService = userService;
-	}
+    public UsersController(IUserService userService, IJobService jobService)
+    {
+        _jobService = jobService;
+        _userService = userService;
+    }
 
-	public IActionResult Create()
-	{
-		return View();
-	}
+    public IActionResult Create()
+    {
+        return View();
+    }
 
-	[HttpPost]
-	[ValidateAntiForgeryToken]
-	public async Task<IActionResult> Create([Bind("Id,Email,Password,Name,Type")] User user)
-	{
-		if (ModelState.IsValid)
-		{
-			_userService.CreateAsync(user);
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Id,Email,Password,Name,Type")] User user)
+    {
+        if (ModelState.IsValid)
+        {
+            _userService.CreateAsync(user);
 
-			return RedirectToAction("Login");
-		}
+            return RedirectToAction("Login");
+        }
 
-		return View(user);
-	}
+        return View(user);
+    }
 
-	public IActionResult Login()
-	{
-		return View();
-	}
+    public IActionResult Login()
+    {
+        return View();
+    }
 
-	[HttpPost]
-	[ValidateAntiForgeryToken]
-	public async Task<IActionResult> Login(string email, string password)
-	{
-		if (ModelState.IsValid)
-		{
-			var user = await _userService.LoginAsync(email, password);
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Login(string email, string password)
+    {
+        if (ModelState.IsValid)
+        {
+            var user = await _userService.LoginAsync(email, password);
 
-			if (user == null)
-			{
-				ModelState.AddModelError("", "Email ou senha inválidos.");
+            if (user == null)
+            {
+                ModelState.AddModelError("", "Email ou senha inválidos.");
 
-				return View();
-			}
+                return View();
+            }
 
-			var claims = new List<Claim>
-			{
-				new(ClaimTypes.Email, user.Email),
-				new(ClaimTypes.Name, user.Name),
-				new(ClaimTypes.Role, user.Type.ToString())
-			};
+            var claims = new List<Claim>
+            {
+                new(ClaimTypes.Email, user.Email),
+                new(ClaimTypes.Name, user.Name),
+                new(ClaimTypes.Role, user.Type.ToString())
+            };
 
-			var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+            var claimsIdentity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
 
-			await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
+            await HttpContext.SignInAsync(CookieAuthenticationDefaults.AuthenticationScheme, new ClaimsPrincipal(claimsIdentity));
 
-			return RedirectToAction("Index", "Home");
-		}
+            return RedirectToAction("Index", "Home");
+        }
 
-		return View();
-	}
+        return View();
+    }
 
-	public async Task<IActionResult> Logout()
-	{
-		await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    public async Task<IActionResult> Logout()
+    {
+        await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
 
-		return RedirectToAction("Index", "Home");
-	}
+        return RedirectToAction("Index", "Home");
+    }
+
+    [Authorize]
+    public async Task<IActionResult> GetWorkers()
+    {
+        var workers = await _userService.GetWorkersAsync();
+
+        return View(workers);
+    }
 }
