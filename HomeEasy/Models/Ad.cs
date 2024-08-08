4f2c435eb5f1ced85b9b45ba93fc7ff1113078d9
Vitor Ravenna Faccio <5by5.vfaccio@voeazul.com.br>
2024-08-08 17:16:14 -0300

    Cria novo objeto Ad

---
+++ HomeEasy/Models/Ad.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeEasy.Models;
+
+public sealed class Ad
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Display(Name = "Título")]
+    [Required(ErrorMessage = "Título é obrigatório")]
+    public string Title { get; set; }
+
+    [Display(Name = "Descrição")]
+    [Required(ErrorMessage = "Descrição é obrigatório")]
+    public string Description { get; set; }
+
+    [Display(Name = "Trabalho")]
+    [Required(ErrorMessage = "Selecione pelo menos um trabalho")]
+    public string Job { get; set; }
+
+    public DateTime StartDate { get; set; }
+
+    public DateTime EndDate { get; set; }
+}
