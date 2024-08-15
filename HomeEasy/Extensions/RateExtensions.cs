90cd8f2778f42b6908c0ad41d772224d2a66309c
Vitor Ravenna Faccio <5by5.vfaccio@voeazul.com.br>
2024-08-15 11:20:15 -0300

    Cria RateExtensions

---
+++ HomeEasy/Extensions/RateExtensions.cs
@@ -0,0 +1,14 @@
+﻿using HomeEasy.Models;
+
+namespace HomeEasy.Extensions;
+
+public static class RateExtensions
+{
+    public static int GetUserRating(this List<Review> reviews)
+    {
+        if (!reviews?.Any() ?? true)
+            return 0;
+
+        return (int)Math.Round(reviews.Select(review => review.Rating).Average());
+    }
+}
