21b26f5ff3d84b215485e979f1f3e7c747623276
Vitor Ravenna Faccio <5by5.vfaccio@voeazul.com.br>
2024-08-15 11:18:46 -0300

    Cria contract service

---
+++ HomeEasy/Interfaces/IContractService.cs
@@ -0,0 +1,22 @@
+﻿using HomeEasy.Models;
+
+namespace HomeEasy.Interfaces;
+
+public interface IContractService
+{
+    Task CreateAsync(Contract contract);
+
+    Task<List<Contract>?> GetUserNotCompletedApprovedContractsAsync(string userId);
+
+    Task<List<Contract>?> GetUserNotCompletedPendingContractsAsync(string userId);
+
+    Task<List<Contract>?> GetUserCompletedContractsAsync(string userId);
+
+    Task<Contract?> GetContractById(Guid id);
+
+    Task ApproveContractAsync(Guid id);
+
+    Task CompleteContractAsync(Guid id);
+
+    Task DeleteContractAsync(Guid id);
+}
