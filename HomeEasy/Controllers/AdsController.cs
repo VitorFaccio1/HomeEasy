18dda5f13cb52e3c27272898434e9af3e710e85b
Vitor Ravenna Faccio <5by5.vfaccio@voeazul.com.br>
2024-08-15 11:19:47 -0300

    Remove SetGoBack

--- HomeEasy/Controllers/AdsController.cs
+++ HomeEasy/Controllers/AdsController.cs
@@ -6,173 +6,143 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Security.Claims;
 
-namespace HomeEasy.Controllers
+namespace HomeEasy.Controllers;
+
+[Authorize]
+public class AdsController : Controller
 {
-    [Authorize]
-    public class AdsController : Controller
+    private readonly IAdService _adService;
+    private readonly IJobService _jobService;
+    private readonly IUserService _userService;
+
+    public AdsController(
+        IAdService adService,
+        IJobService jobService,
+        IUserService userService)
     {
-        private readonly IAdService _adService;
-        private readonly IJobService _jobService;
-        private readonly IUserService _userService;
+        _jobService = jobService;
+        _adService = adService;
+        _userService = userService;
+    }
 
-        public AdsController(
-            IAdService adService,
-            IJobService jobService,
-            IUserService userService)
+    public async Task<IActionResult> Clients(int page = 1)
+    {
+        var size = 9;
+
+        var clientsAds = await _adService.GetClientsNotExpiredAdsAsync(page, size);
+
+        var clientAdsTotalCount = await _adService.GetNotExpiredAdsTotalCountByUserTypeAsync(UserType.Client);
+
+        ViewBag.CurrentPage = page;
+        ViewBag.TotalPages = (int)Math.Ceiling(clientAdsTotalCount / (double)size);
+
+        return View(clientsAds);
+    }
+
+    public async Task<IActionResult> Workers(int page = 1)
+    {
+        var size = 9;
+
+        var workerAds = await _adService.GetWorkersNotExpiredAdsAsync(page, size);
+        var workersAdsTotalCount = await _adService.GetNotExpiredAdsTotalCountByUserTypeAsync(UserType.Worker);
+
+        ViewBag.CurrentPage = page;
+        ViewBag.TotalPages = (int)Math.Ceiling(workersAdsTotalCount / (double)size);
+
+        return View(workerAds);
+    }
+
+    public async Task<IActionResult> Details(Guid? id)
+    {
+        var ad = await _adService.GetAdAsync(id);
+
+        return ad != null
+            ? View(ad)
+            : NotFound();
+    }
+
+    public async Task<IActionResult> Create()
+    {
+        ViewBag.Jobs = await _jobService.GetJobsAsync();
+
+        return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Create([Bind("Title,Description,Job")] Ad ad)
+    {
+        if (ModelState.IsValid)
         {
-            _jobService = jobService;
-            _adService = adService;
-            _userService = userService;
+            var user = await _userService.GetUserByIdAsync(User.FindFirst(ClaimTypes.SerialNumber)?.Value);
+
+            await _adService.CreateAsync(ad, user);
+
+            return User.IsInRole(UserType.Worker.GetDescription())
+                ? RedirectToAction(nameof(Workers))
+                : RedirectToAction(nameof(Clients));
         }
 
-        public async Task<IActionResult> Clients(int page = 1)
+        return RedirectToAction(nameof(Create));
+    }
+
+    public async Task<IActionResult> Edit(Guid? id, int page = 1)
+    {
+        ViewBag.Jobs = await _jobService.GetJobsAsync();
+        ViewBag.Page = page;
+
+        var ad = await _adService.GetAdAsync(id);
+
+        return ad != null
+            ? View(ad)
+            : NotFound();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> Edit([Bind("Id, Title,Description,Job")] Ad ad, Guid id, int page = 1)
+    {
+        if (id != ad.Id)
+            return NotFound();
+
+        if (ModelState.IsValid)
         {
-            var size = 9;
+            await _adService.EditAdAsync(ad);
 
-            var clientsAds = await _adService.GetClientsNotExpiredAdsAsync(page, size);
-
-            var clientAdsTotalCount = await _adService.GetNotExpiredAdsTotalCountByUserTypeAsync(UserType.Client);
-
-            ViewBag.CurrentPage = page;
-            ViewBag.TotalPages = (int)Math.Ceiling(clientAdsTotalCount / (double)size);
-
-            return View(clientsAds);
-        }
-
-        public async Task<IActionResult> Workers(int page = 1)
-        {
-            var size = 9;
-
-            var workerAds = await _adService.GetWorkersNotExpiredAdsAsync(page, size);
-            var workersAdsTotalCount = await _adService.GetNotExpiredAdsTotalCountByUserTypeAsync(UserType.Worker);
-
-            ViewBag.CurrentPage = page;
-            ViewBag.TotalPages = (int)Math.Ceiling(workersAdsTotalCount / (double)size);
-
-            return View(workerAds);
-        }
-
-        public async Task<IActionResult> Details(Guid? id, int page, UserType viewType)
-        {
-            var ad = await _adService.GetAdAsync(id);
-
-            ViewBag.Page = page;
-
-            SetGoBack(viewType);
-
-            return ad != null
-                ? View(ad)
-                : NotFound();
-        }
-
-        public async Task<IActionResult> Create()
-        {
-            ViewBag.Jobs = await _jobService.GetJobsAsync();
-
-            return View();
-        }
-
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Create([Bind("Title,Description,Job")] Ad ad)
-        {
-            if (ModelState.IsValid)
+            return RedirectToAction("MyAds", "Users", new
             {
-                var user = await _userService.GetUserByIdAsync(User.FindFirst(ClaimTypes.SerialNumber)?.Value);
-
-                await _adService.CreateAsync(ad, user);
-
-                return User.IsInRole(UserType.Worker.GetDescription())
-                    ? RedirectToAction(nameof(Workers))
-                    : RedirectToAction(nameof(Clients));
-            }
-
-            return RedirectToAction(nameof(Create));
+                Page = page
+            });
         }
 
-        public async Task<IActionResult> Edit(Guid? id, int page = 1)
+        return View(ad);
+    }
+
+    public async Task<IActionResult> Delete(Guid? id, int page = 1)
+    {
+        var ad = await _adService.GetAdAsync(id);
+        ViewBag.Page = page;
+
+        return ad != null
+            ? View(ad)
+            : NotFound();
+    }
+
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public async Task<IActionResult> DeleteConfirmed(Guid id)
+    {
+        var ad = await _adService.GetAdAsync(id);
+
+        if (ad != null)
         {
-            ViewBag.Jobs = await _jobService.GetJobsAsync();
-            ViewBag.Page = page;
+            await _adService.DeleteAdAsync(ad);
 
-            var ad = await _adService.GetAdAsync(id);
-
-            return ad != null
-                ? View(ad)
-                : NotFound();
+            return ad.EndDate >= DateTime.Now
+                ? RedirectToAction("MyAds", "Users")
+                : RedirectToAction("MyExpiredAds", "Users");
         }
 
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit([Bind("Id, Title,Description,Job")] Ad ad, Guid id, int page = 1)
-        {
-            if (id != ad.Id)
-                return NotFound();
-
-            if (ModelState.IsValid)
-            {
-                await _adService.EditAdAsync(ad);
-
-                return RedirectToAction("MyAds", "Users", new
-                {
-                    Page = page
-                });
-            }
-
-            return View(ad);
-        }
-
-        public async Task<IActionResult> Delete(Guid? id, int page = 1)
-        {
-            var ad = await _adService.GetAdAsync(id);
-            ViewBag.Page = page;
-
-            SetGoBack(null);
-
-            return ad != null
-                ? View(ad)
-                : NotFound();
-        }
-
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public async Task<IActionResult> DeleteConfirmed(Guid id)
-        {
-            var ad = await _adService.GetAdAsync(id);
-
-            if (ad != null)
-            {
-                await _adService.DeleteAdAsync(ad);
-
-                return ad.EndDate >= DateTime.Now 
-                    ? RedirectToAction("MyAds", "Users") 
-                    : RedirectToAction("MyExpiredAds", "Users");
-            }
-
-            return View(ad);
-        }
-
-        private void SetGoBack(UserType? viewType)
-        {
-            ViewBag.Action = Path.GetFileName(Request.Headers.Referer.ToString());
-
-            if (ViewBag.Action.Contains("MyAds"))
-            {
-                ViewBag.Controller = "Users";
-                ViewBag.Action = "MyAds";
-            }
-            else if (ViewBag.Action.Contains("MyExpiredAds"))
-            {
-                ViewBag.Controller = "Users";
-                ViewBag.Action = "MyExpiredAds";
-            }
-            else
-            {
-                ViewBag.Controller = "Ads";
-                ViewBag.Action = viewType == UserType.Client
-                    ? "Clients"
-                    : "Workers";
-            }
-        }
+        return View(ad);
     }
 }
