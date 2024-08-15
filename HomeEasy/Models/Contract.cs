d22f244d028e3001177385bc4c42d31e9409ed3e
Vitor Ravenna Faccio <5by5.vfaccio@voeazul.com.br>
2024-08-15 11:20:23 -0300

    Adiciona Contract

---
+++ HomeEasy/Models/Contract.cs
@@ -0,0 +1,34 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HomeEasy.Models;
+
+public class Contract
+{
+    public Contract()
+    { }
+
+    public Contract(Ad ad, User? contractor)
+    {
+        Id = Guid.NewGuid();
+        Ad = ad;
+        Contractee = ad.User;
+        Contractor = contractor;
+    }
+
+    [Key]
+    public Guid Id { get; set; }
+
+    public Ad Ad { get; set; }
+
+    public User Contractor { get; set; }
+
+    public User Contractee { get; set; }
+
+    public bool Approved { get; set; }
+
+    public bool Completed { get; set; }
+
+    public bool ContracteeReviewed { get; set; }
+
+    public bool ContractorReviewed { get; set; }
+}
